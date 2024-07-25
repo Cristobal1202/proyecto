e56505e3300710555d7048f8bf700edf97076b35
CRISTOBAL ANDRES MARTINEZ RETAMAL <cristobal.martinez_r@mail.udp.cl>
2024-07-25 12:45:21 -0400

    cambio de sqlite a postgresql

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database',
+        'USER': 'cristobal',
+        'PASSWORD': 'joker120',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
